8b0cff65a481eb578f11d6fc063d181f06d54a79
Zukron Alviandy <zukronalviandy@gmail.com>
2024-12-12 12:13:49 +0700

    feat: book middleware

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => redirect()->route('book.index'));
 
+// Auth
 Route::get('/login', [AuthController::class, 'loginForm'])->name('auth.loginForm');
 Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
 
 Route::get('/register', [AuthController::class, 'registerForm'])->name('auth.registerForm');
 Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+
+// Book
+Route::resource('book', BookController::class)->middleware('auth');
